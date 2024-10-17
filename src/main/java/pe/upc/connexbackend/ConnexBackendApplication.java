5e5e3143d32a92bdb3b37b1e117419e1d273c50b
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-10-17 15:59:25 -0500

    Initial commit

---
+++ src/main/java/pe/upc/connexbackend/ConnexBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.connexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/upc/connexbackend/ConnexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.connexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
