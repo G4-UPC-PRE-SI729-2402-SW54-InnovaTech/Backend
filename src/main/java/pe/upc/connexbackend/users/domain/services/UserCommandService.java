c1287370bde6d10bbff9ddad1fb39b84f6f2bd35
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-10-17 18:57:20 -0500

    feat: complete user services

--- src/main/java/pe/upc/connexbackend/users/domain/services/UserCommandService.java
+++ src/main/java/pe/upc/connexbackend/users/domain/services/UserCommandService.java
@@ -1,4 +1,12 @@
 package pe.upc.connexbackend.users.domain.services;
 
+import pe.upc.connexbackend.users.domain.model.commands.CreateUserCommand;
+import pe.upc.connexbackend.users.domain.model.aggregates.User;
+import pe.upc.connexbackend.users.domain.model.commands.DeleteUserCommand;
+import pe.upc.connexbackend.users.domain.model.commands.UpdateUserCommand;
+
 public interface UserCommandService {
+    User handle(CreateUserCommand command);
+    User handle(DeleteUserCommand command);
+    User handle(UpdateUserCommand command);
 }
--- src/main/java/pe/upc/connexbackend/users/domain/services/UserQueryService.java
+++ src/main/java/pe/upc/connexbackend/users/domain/services/UserQueryService.java
@@ -1,4 +1,18 @@
 package pe.upc.connexbackend.users.domain.services;
 
+import pe.upc.connexbackend.users.domain.model.aggregates.User;
+import pe.upc.connexbackend.users.domain.model.queries.GetAllUsersQuery;
+import pe.upc.connexbackend.users.domain.model.queries.GetUserByEmailQuery;
+import pe.upc.connexbackend.users.domain.model.queries.GetUserByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserQueryService {
+
+    Optional<User> handle(GetUserByEmailQuery query);
+
+    Optional<User> handle(GetUserByIdQuery query);
+
+    List<User> handle(GetAllUsersQuery query);
 }
