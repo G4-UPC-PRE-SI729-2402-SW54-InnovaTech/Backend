036f46932442ce4fe85e7a9f2f697603fa4ab2d7
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-10-29 11:31:23 -0500

    Update GetUserByIdQuery.java

--- src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetUserByIdQuery.java
+++ src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetUserByIdQuery.java
@@ -1,8 +1,10 @@
 package pe.upc.connexbackend.users.domain.model.queries;
 
-public record GetUserByIdQuery (
-        Integer id
+// Define un registro inmutable para consultar una entidad de User por su ID
+public record GetUserByIdQuery(
+        Integer id // ID del usuario a consultar
 ) {
+    // Constructor validado que lanza una excepción si el ID es nulo
     public GetUserByIdQuery {
         if (id == null) {
             throw new IllegalArgumentException("id is required");
