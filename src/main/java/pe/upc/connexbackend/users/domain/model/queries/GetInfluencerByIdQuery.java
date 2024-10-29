8560f2c66979bd5473d07f3a4c65dc178569ef28
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-10-29 11:30:44 -0500

    Update GetInfluencerByIdQuery.java

--- src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetInfluencerByIdQuery.java
+++ src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetInfluencerByIdQuery.java
@@ -1,6 +1,9 @@
 package pe.upc.connexbackend.users.domain.model.queries;
 
+// Define un registro inmutable para consultar una entidad de Influencer por su ID
 public record GetInfluencerByIdQuery(Integer influencerId) {
+    
+    // Constructor validado que lanza una excepción si el ID del influencer es nulo
     public GetInfluencerByIdQuery {
         if (influencerId == null) {
             throw new IllegalArgumentException("influencerId is required");
