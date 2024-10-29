38c34818338de3ccfb66d10a7532bbb9bcea58b2
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-10-29 11:31:44 -0500

    Update EmailAddress.java

--- src/main/java/pe/upc/connexbackend/users/domain/model/valueobjects/EmailAddress.java
+++ src/main/java/pe/upc/connexbackend/users/domain/model/valueobjects/EmailAddress.java
@@ -1,16 +1,20 @@
 package pe.upc.connexbackend.users.domain.model.valueobjects;
 
+// Indica que esta clase puede ser embebida en otras entidades JPA
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record EmailAddress(String address) {
-    public EmailAddress(){
+    
+    // Constructor por defecto que inicializa el address como nulo
+    public EmailAddress() {
         this(null);
     }
-    public EmailAddress{
-        if (address == null || address.isBlank()){
+
+    // Constructor validado que lanza una excepción si la dirección de correo electrónico es nula o está vacía
+    public EmailAddress {
+        if (address == null || address.isBlank()) {
             throw new IllegalArgumentException("Email address is required");
         }
     }
-
 }
