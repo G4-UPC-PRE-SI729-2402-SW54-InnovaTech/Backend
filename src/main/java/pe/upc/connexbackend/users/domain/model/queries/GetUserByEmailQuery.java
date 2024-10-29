80f34b913c373d0ced17d2f4dcca78438b94878f
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-10-29 11:31:05 -0500

    Update GetUserByEmailQuery.java

--- src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetUserByEmailQuery.java
+++ src/main/java/pe/upc/connexbackend/users/domain/model/queries/GetUserByEmailQuery.java
@@ -1,8 +1,12 @@
 package pe.upc.connexbackend.users.domain.model.queries;
 
+// Importa la clase EmailAddress del modelo de valor
 import pe.upc.connexbackend.users.domain.model.valueobjects.EmailAddress;
 
+// Define un registro inmutable para consultar una entidad de User por su dirección de correo electrónico
 public record GetUserByEmailQuery(EmailAddress emailAddress) {
+    
+    // Constructor validado que lanza una excepción si la dirección de correo electrónico es nula
     public GetUserByEmailQuery {
         if (emailAddress == null) {
             throw new IllegalArgumentException("emailAddress is required");
